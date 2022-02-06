247d07fbee6b5151a701ae38fa639dcbdbbdb3e8
hretska <hretskauni@gmail.com>
2022-02-06 22:04:02 +0200

    1

---
+++ java-2022/lab1/lab1/src/main/java/phones/phones.java
@@ -0,0 +1,46 @@
+package phones;
+
+public class phones {
+
+	private String title;
+	private int power;
+	private float price;
+	
+	public phones(String title, int power, float price) {
+		super();
+		this.title = title;
+		this.power = power;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "{\"title\": \""+title+"\",\"power\": "+power+", \"price\": "+price+"}";
+	}
+	
+	
+}
---
+++ java-2022/lab1/lab1/src/main/java/servlet/Servlet1.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import phones.phones;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("application/json");
+		
+		PrintWriter out = response.getWriter();
+		
+		phones phones = new phones("phones1", 80000, 12000f);
+		
+		out.println("["+phones+"]");
+	}
+
+}
